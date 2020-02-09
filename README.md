5b2eb35c4bfc001148865ccd2163e522ee0401f0
Amal Abeygunawardana <amalabey@gmail.com>
2020-02-09 21:37:20 +1100

    Add fluentmigrator support

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # multitenancy-sample
 Multi-tenant ASP.NET Core Application with Dotnettency
+
+
+# Run DB Migrations
+```
+dotnet fm migrate -p SqlServer -c "Data Source=.\sqlexpress;Initial Catalog=todo-fabrikam;Integrated Security=True" -a .\bin\Debug\netstandard2.0\TodoApp.Data.dll up -t 100
+```
---
+++ TodoApp/TodoApp.Data/Migrations/TenantDb/100_InitialCreate.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace TodoApp.Data.Migrations.TenantDb
+{
+    [Migration(100)]
+    public class AddLogTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("TodoItems")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Description").AsString()
+                .WithColumn("IsComplete").AsBoolean();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("TodoItems");
+        }
+    }
+}
